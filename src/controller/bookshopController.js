bd29e14402c5e9d66d41941b6811275059b5bfd2
marianavns <mariana.vns@gmail.com>
2020-11-11 13:15:44 -0300

    Add routes to POST and GET

--- src/controller/bookshopController.js
+++ src/controller/bookshopController.js
@@ -1,73 +1,43 @@
-const books = require( '../model/books.json' )
-const collaborators = require( '../model/collaborators.json' )
+const books = require( '../model/books.js' )
+// const collaborators = require( '../model/collaborators.js' )
 
 const fs = require('fs')
 
-const getAllBooks = ( req, res ) => {
-    res.status(200).send(books)
-}
-
-const postBook = ( req, res ) => {
-    const {id, isbn, name, authorship, publishingCompany, release, genre} = req.body
-    books.push({id, isbn, name, authorship, publishingCompany, release, genre})
-    fs.writeFile('./src/model/books.json', JSON.stringify(books), 'utf8', function(err){
-       if (err) {
-         return res.status(424).send({message: err});
-       }
-        console.log("O arquivo foi atualizado.")
-    })    
-    res.status(200).send(books)
-}
-
-const deleteBook = ( req, res ) => {
-    const id = req.param.id
-    const filteredBook = books.filter((array) => array.id == id)
-    const indexFilteredBook = books.indexOf(filteredBook)
-    books.splice(indexFilteredBook)
-    fs.writeFile('./src/model/books.json', JSON.stringify(books), 'utf8', function(err){
+const getAllBooks = (req, res) => {
+    books.find(function (err, books) {
         if (err) {
-        return res.status(424).send({message: err});
+            res.status(500).send({ message: err.message })
+        } else {
+            res.status(200).send(books)
         }
-        console.log("O arquivo foi atualizado.")
     })
-res.status(200).send(books)
 }
 
-const getAllCollaborators = ( req, res ) => {
-    res.status(200).send(collaborators)
-}
+const getBySearch = (req, res) => {
+    const search = req.query
+    books.find(search, function (err, books) {
+        if (err) {
+            res.status(500).send({ message: err.message })
+        } else {
+            console.log(`Você fez uma busca.`)
+            res.status(200).send(books)
+        }
+    })
+}   
 
-// const postCollaborator = ( req, res ) => {
-//     res.status(200).send(collaboratorNewList)
-// }
-
-// const deleteCollaborator = ( req, res ) => {
-//     res.status(200).send(collaborators)
-// }
-
-/////////////////
-
-// const getBookByGenre = ( req, res ) => {
-//     res.status(200).send(booksByGenre)
-// }
-
-const getAgeByID = ( req, res ) => {
-    const actualYear = 2020
-        const id = req.params.id
-        const filtered = collaborators.find((param) => param.id == id)
-        const name = filtered.name
-        const yearBirth = filtered.id.slice(4, -4)
-    const age = actualYear - yearBirth
-res.status(200).send(`A pessoa com o id ${id} se chama ${name} e tem ${age} anos.`)
+const postBook = (req, res) => {
+    let book = new books(req.body)
+    book.save(function(err){
+        if (err) {
+            res.status(500).send({ message: err.message })
+        } else {
+            res.status(201).send({ message : "livro incluído com sucesso"})
+        }
+    })
 }
 
 module.exports = { 
+    getBySearch,
     getAllBooks,
-    postBook, 
-    deleteBook, 
-    getAllCollaborators,
-    // postCollaborator,
-    // deleteCollaborator,
-    // getBookByGenre,
-    getAgeByID
+    postBook
 }
--- src/router/bookshopRouter.js
+++ src/router/bookshopRouter.js
@@ -1,21 +1,11 @@
-
-// Onde relacionamos as rotas
-
 const controller = require('../controller/bookshopController')
 
 const express = require('express')
 
 const router = express.Router()
 
-
+router.get('/:search', controller.getBySearch)
 router.get('/livros', controller.getAllBooks)
-router.post('/adicionarlivro', controller.postBook)
-router.delete('/excluirlivro/:id', controller.deleteBook)
-router.get('/colaboradores', controller.getAllCollaborators)
-// router.post('/adicionarcolaborador'), controller.postCollaborator
-// router.delete('/colaborador/:id', controller.deleteCollaborator)
-
-// router.get('/livros/:genre', controller.getBookByGenre)
-router.get('/idade/:id', controller.getAgeByID)
+router.post('/livros/adicionarlivro', controller.postBook)
 
 module.exports = router
